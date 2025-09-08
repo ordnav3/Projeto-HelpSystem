bcbf7e757f67796ad9f040ea176ae172b281f9bd
ordnav3 <evandrosantosafonso@live.com>
2025-09-08 14:18:00 -0300

    Structuring the project

---
+++ help-system/src/routes.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import CreateRequest from "./pages/CreateRequest";
+// import RequestsList from "./pages/RequestsList";
+
+const AppRoutes: React.FC = () => {
+    return (
+        <Routes>
+            {/* <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/create-request" element={<CreateRequest />} />
+            <Route path="/requests" element={<RequestsList />} /> */}
+        </Routes>
+    );
+};
+
+export default AppRoutes;
+
